2cf6b65e838d9a15094222716c339aaefbaadfa8
haeun1107 <haeun9394@gmail.com>
2025-07-15 20:46:29 +0800

    [Fix] #0 -  axios 요청 시 Authorization 헤더 자동 설정에서 로그인 요청은 예외 처리

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -8,7 +8,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
-  if (token) {
+  if (token && config.url !== "/admin/login") {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
